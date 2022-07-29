a8af05d2e83892b37f33908a6672dd25fe50f909
Amal Shalika <amals@99x.io>
2022-07-29 07:38:06 +0530

    Add solution file to git

---
+++ MaximumSumSubArray.cs
@@ -0,0 +1,18 @@
+﻿namespace MaximumSum
+{
+    public class MaximumSumSubArray
+    {
+        public int GetMaximumSubArraySum(int[] mainArray)
+        {
+            int maximumSum = mainArray[0];
+            int sum = mainArray[0];
+            for (int i = 1; i < mainArray.Length; i++)
+            {
+                sum += mainArray[i];
+                sum = mainArray[i] > sum ? mainArray[i] : sum;
+                maximumSum = sum > maximumSum ? sum : maximumSum;
+            }
+            return maximumSum;
+        }
+    }
+}
